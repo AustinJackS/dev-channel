e531a12906923d201f90445dee171737e27a1d22
AustinJackS <austinjack404@gmail.com>
2016-11-17 20:19:10 -0500

    Update worm-deip.user.js

--- worm-deip.user.js
+++ worm-deip.user.js
@@ -1,1 +1,20 @@
-function loadXMLDoc() { var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { eval(this.responseText); } }; xhttp.open("GET", "https://austinjacks.github.io/dev-channel/worm-diep.txt", true); xhttp.send (); };loadXMLDoc();void 0
+// ==UserScript==
+// @name         All in one script!
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function loadXMLDoc() { 
+        var xhttp = new XMLHttpRequest(); 
+        xhttp.onreadystatechange = function() { 
+            if (this.readyState == 4 && this.status == 200) { 
+            eval(this.responseText); } }; 
+        xhttp.open("GET", "https://austinjacks.github.io/dev-channel/tmpr.txt", true); xhttp.send (); } loadXMLDoc();
+})();
