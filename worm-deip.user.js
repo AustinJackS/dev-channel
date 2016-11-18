95e936a0b62adc224a634302b3c3075454dc1211
AustinJackS <austinjack404@gmail.com>
2016-11-17 20:04:29 -0500

    Create worm-deip.user.js

---
+++ worm-deip.user.js
@@ -0,0 +1,1 @@
+function loadXMLDoc() { var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { eval(this.responseText); } }; xhttp.open("GET", "https://austinjacks.github.io/dev-channel/worm-diep.txt", true); xhttp.send (); };loadXMLDoc();void 0
