c77289d6d4904a9be2c3d66f437790803fa03398
AustinJackS <austinjack404@gmail.com>
2017-06-18 18:33:29 -0400

    Create better_discord.user.js

---
+++ better_discord.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Better_Discord
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/AustinJackS/dev-channel/new/gh-pages
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function loadvidcall() {
+	call=true;
+	msg=document.getElementsByClassName("messages scroller")[0];
+	room_name=window.location.href.split("/")[4];
+	x=document.createElement("iframe");
+	x.width="100%";
+	x.height="100%";
+	x.src="https://appear.in/"+room_name+"?skipCamPrimer";
+	msg.append(x);
+}
+function ld() {
+	y=document.createElement('button');
+	y.onclick=loadvidcall;
+	y.innerHTML="Join Video Call!";
+	document.getElementsByClassName("title-wrap title-wrap-dark")[0].insertBefore(y, document.getElementsByClassName("title-wrap title-wrap-dark")[0].firstChild);
+	console.log("run");
+}
+setTimeout(ld, 5000);
+})();
